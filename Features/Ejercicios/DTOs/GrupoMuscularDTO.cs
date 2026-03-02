884c41e238a9812de5037eac3092c7865d866aa5
Lisandro <lisandrosemperez@gmail.com>
2026-03-02 16:19:30 -0300

    fix: Propiedad Ids eliminados de DTOS

--- Features/Ejercicios/DTOs/GrupoMuscularDTO.cs
+++ Features/Ejercicios/DTOs/GrupoMuscularDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace AnotadorGymAppApi.Features.Ejercicios.DTOs
 {
     public class GrupoMuscularDTO
-    {        
-        public int GrupoMuscularId { get; set; }
+    {                
         public string Nombre { get; set; } = string.Empty;
         public GrupoMuscularDTO() { }
 
--- Features/Ejercicios/DTOs/MusculoDTO.cs
+++ Features/Ejercicios/DTOs/MusculoDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace AnotadorGymAppApi.Features.Ejercicios.DTOs
 {
     public class MusculoDTO
-    {        
-        public int MusculoId { get; set; }
+    {                
         public string Nombre { get; set; } = string.Empty;
         public MusculoDTO() { }
     }
