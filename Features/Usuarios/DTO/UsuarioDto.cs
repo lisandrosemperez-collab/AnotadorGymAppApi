0623f108fb8a726488374a98c79b8a0d2a7aa4b3
Lisandro <lisandrosemperez@gmail.com>
2026-04-14 13:53:12 -0300

    fix: Elimina Rol del DTO de login: el rol debe obtenerse de DB por seguridad

--- Features/Usuarios/DTO/UsuarioDto.cs
+++ Features/Usuarios/DTO/UsuarioDto.cs
@@ -3,7 +3,6 @@
     public class UsuarioDto
     {
         public string UserName { get; set; }        
-        public string Password { get; set; }
-        public string Rol {  get; set; }
+        public string Password { get; set; }        
     }
 }
